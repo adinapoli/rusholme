d65cf06af41c6a48a04c6d34cebdd9ac96f1bfb0
Alfredo Di Napoli <alfredo@well-typed.com>
2026-05-22 06:47:04 +0000

    #605: Allocate thunks with minimum 1 field for update frames

--- tests/e2e_test_runner.zig
+++ tests/e2e_test_runner.zig
@@ -442,3 +442,7 @@ test "e2e: e2e_show_nonascii_char (#682)" {
 test "e2e: e2e_679_deriving_eq (#679)" {
     try testE2e(std.testing.allocator, "e2e_679_deriving_eq");
 }
+
+test "e2e: e2e_605_zero_field_thunk (#605)" {
+    try testE2e(std.testing.allocator, "e2e_605_zero_field_thunk");
+}
